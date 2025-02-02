30b688d0518a53ade71a7fca1004091bc7152137
kareem shetta <kareemshetta8@gmail.com>
2025-02-02 21:55:05 +0200

    update-uploads

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { upload } from "./utils/upload";
+import express from "express";
 import { Application, Request, Response } from "express";
 import userRoute from "./modules/users/versions.routes";
 import authRoute from "./modules/auth/versions.routes";
@@ -9,11 +9,14 @@ import attributesRoute from "./modules/attributes/versions.routes";
 import productsRoute from "./modules/products/versions.routes";
 import uploadsRoute from "./modules/uploads/versions.routes";
 import cartsRoute from "./modules/carts/versions.routes";
+import path from "path";
 export const injectRoutes = (app: Application): void => {
   // Basic route
   app.get("/", (req: Request, res: Response) => {
     res.json({ message: "Hello from MasterTech!" });
   });
+
+  app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
   app.use("/users", userRoute);
   app.use("/auth", authRoute);
   app.use("/stores", storeRoute);
