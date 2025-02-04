699f542264065775782cc15e68ca6664b1d6d573
kareem shetta <kareemshetta8@gmail.com>
2025-02-05 01:18:22 +0200

    add-cors

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const i18n_1 = __importDefault(require("./config/i18n"));
 const i18next_http_middleware_1 = __importDefault(require("i18next-http-middleware"));
 const notFound_middleware_1 = require("./middlewares/notFound.middleware");
@@ -16,6 +17,7 @@ const routes_1 = require("./routes");
 const app = (0, express_1.default)();
 // Middleware to parse JSON bodies
 app.use((0, morgan_1.default)("dev"));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(passport_1.default.initialize());
 app.use(i18next_http_middleware_1.default.handle(i18n_1.default));
